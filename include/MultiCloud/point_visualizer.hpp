43ddbe375e4288619d6ec62d514b4298e5e1bbd6
다넨드라 <danendraclever24@gmail.com>
2024-11-29 23:19:38 +0700

    Feat: Added base classes for visualizer

--- include/MultiCloud/point_visualizer.hpp
+++ include/MultiCloud/point_visualizer.hpp
@@ -1,8 +1,14 @@
+#ifndef MULTICLOUD_VISUALIZER_HPP_
+#define MULTICLOUD_VISUALIZER_HPP_
+
 #pragma once
+#include <cstdint>
 
 #include <X11/Xlib.h>
 #include <GL/glx.h>
 #include <GL/glext.h>
+#include <MultiCloud/point_visualizer.hpp>
+#include "MultiCloud/types.hpp"
 
 typedef GLXContext (*glXCreateContextAttribsARBProc)
   (Display*, GLXFBConfig, GLXContext, Bool, const int*);
@@ -25,5 +31,48 @@ const char *fragment_shader_src = "#version 330 core\n"
     "{\n"
     "   FragColor = vec4(0.0f, 1.0f, 0.0f, 1.0f);\n"
     "}\0";
+ 
+class Visualizer {
+  public:
+    Visualizer();
+    ~Visualizer();
 
-/* class PointVisualizer : Public */
+    void Spin(const uint16_t freq);
+    bool IsStopped();
+    
+    /**
+     * @brief size in meter scale
+     */
+    void CreateCoordSystem(const uint16_t size);
+    
+    /**
+     * @brief size_x in meter scale
+     * @brief size_y in meter scale
+     * @brief size_z in meter scale
+     */
+    void CreateCoordSystem(const uint16_t size_x, const uint16_t size_y, const uint16_t size_z);
+  protected:
+
+  private:
+    void InitWindow();
+};
+
+template<typename CloudType>
+class MultiCloudVisualizer: public CloudSet<CloudType>, Visualizer {
+  public:
+    MultiCloudVisualizer();
+    ~MultiCloudVisualizer();
+  protected:
+  private:
+};
+
+template<typename MeshType>
+class MultiMeshVisualizer : public MultiMesh<MeshType>, Visualizer {
+  public:
+    MultiMeshVisualizer();
+    ~MultiMeshVisualizer();
+  protected:
+  private:
+};
+
+#endif
